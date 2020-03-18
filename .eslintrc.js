d70603659cd17ee0f16ffac29c34a2b5b3a1bd0a
imazurenko <ivan.mazurenko99@gmail.com>
2020-03-18 17:37:20 +0200

    Fix eslint for front-end

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,6 @@
 module.exports = {
     parser: "@typescript-eslint/parser", // Specifies the ESLint parser
     extends: [
-        "airbnb",
         "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
         "plugin:@typescript-eslint/recommended" // Uses the recommended rules from @typescript-eslint/eslint-plugin
     ],
@@ -13,17 +12,14 @@ module.exports = {
         }
     },
     rules: {
-        "semi": [2, "never"],
         "react/prop-types": 0,
         "linebreak-style": 0,
+        "semi": [2, "never"],
         "@typescript-eslint/explicit-function-return-type": 0
     },
     settings: {
         react: {
             version: "detect" // Tells eslint-plugin-react to automatically detect the version of React to use
-        },
-        'import/parsers': {
-            '@typescript-eslint/parser': ['.js', '.ts', '.tsx'],
         }
     }
 };
